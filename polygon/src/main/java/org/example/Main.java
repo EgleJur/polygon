e586f68795149dead56b53fe50e31fa23c895f30
Egle Juraityte <lokajata@gmail.com>
2024-02-27 17:30:46 +0200

    Changed to geometry library. Code is not working

--- polygon/src/main/java/org/example/Main.java
+++ polygon/src/main/java/org/example/Main.java
@@ -1,43 +1,52 @@
 package org.example;
 
+import org.apache.commons.geometry.euclidean.threed.Vector3D;
+import org.apache.commons.geometry.euclidean.threed.rotation.QuaternionRotation;
+import org.apache.commons.math3.geometry.euclidean.twod.Vector2D;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) {
-         Polygon inputPolygon = createInputPolygon();
+        //  List<Vector2D> inputPolygon = createInputPolygon();
 
 //        Polygon inputPolygon = new Polygon();
-//        inputPolygon.addPoint(new Point(0.0, 0.0, 0.0));
-//        inputPolygon.addPoint(new Point(100.0, 0.0, 0.0));
-//        inputPolygon.addPoint(new Point(100.0, 50.0, 0.0));
-//        inputPolygon.addPoint(new Point(0.0, 50.0, 0.0));
+//        inputPolygon.addPoint(new Point(10.0, 10.0, 0.0));
+//        inputPolygon.addPoint(new Point(110.0, 10.0, 0.0));
+//        inputPolygon.addPoint(new Point(110.0, 60.0, 0.0));
+//        inputPolygon.addPoint(new Point(10.0, 60.0, 0.0));
 
-        if (inputPolygon.getPoints().isEmpty() || inputPolygon.size() < 4) {
-            throw new IllegalArgumentException("Polygon must contain at least 4 points.");
-        }
+        List<Vector2D> inputPolygon = new ArrayList<>();
+        inputPolygon.add(new Vector2D(0, 0));
+        inputPolygon.add(new Vector2D(100, 0));
+        inputPolygon.add(new Vector2D(100, 50));
+        inputPolygon.add(new Vector2D(0, 50));
 
         double slopeAngle = 45;
         double azimuthAngle = 270;
 
-        Polygon resultPolygon = create3DPolygon(inputPolygon, slopeAngle, azimuthAngle);
-        System.out.println(resultPolygon);
+        List<Vector3D> transformedPolygon = create3DPolygon(inputPolygon, slopeAngle, azimuthAngle);
+        for (Vector3D vertex : transformedPolygon) {
+            System.out.println(vertex);
+        }
     }
 
-    private static Polygon createInputPolygon() {
+    private static List<Vector2D> createInputPolygon() {
         Scanner scanner = new Scanner(System.in);
-        Polygon polygon = new Polygon();
+        List<Vector2D> polygon = new ArrayList<>();
 
         int i = 0;
         while (i < 4) {
-            System.out.println("Enter coordinates for point " + (i + 1) + " (x y z):");
+            System.out.println("Enter coordinates for point " + (i + 1) + " (x y):");
             double x = scanner.nextDouble();
             double y = scanner.nextDouble();
-            double z = scanner.nextDouble();
 
             boolean pointMatchesPrevious = false;
             for (int j = 0; j < i; j++) {
-                if (polygon.getPoint(j).getX() == x && polygon.getPoint(j).getY() == y &&
-                        polygon.getPoint(j).getZ() == z) {
+                if (polygon.get(j).getX() == x && polygon.get(j).getY() == y) {
                     System.out.println("Point can't be equal to previous points");
                     pointMatchesPrevious = true;
                     break;
@@ -48,7 +57,7 @@ public class Main {
                 continue;
             }
 
-            polygon.addPoint(new Point(x, y, z));
+            polygon.add(new Vector2D(x,y));
             i++;
         }
         scanner.close();
@@ -56,35 +65,57 @@ public class Main {
         return polygon;
     }
 
-    public static Polygon create3DPolygon(Polygon polygon,
+    public static List<Vector3D> create3DPolygon(List<Vector2D> polygon,
                                           double slopeAngle, double azimuthAngle) {
-
+        List<Vector3D> transformedPolygon = new ArrayList<>();
+        for (Vector2D vertex : polygon) {
+            transformedPolygon.add(Vector3D.of(vertex.getX(), vertex.getY(), 0.0));
+        }
         double slopeAngleRad = Math.toRadians(slopeAngle);
-
-        Polygon resultPolygon = new Polygon();
 
         double height = 0.0;
         if (azimuthAngle == 0 || azimuthAngle == 180) {
-            height = Math.tan(slopeAngleRad) * (polygon.getPoint(2).getY() - polygon.getPoint(1).getY());
+            height = Math.tan(slopeAngleRad) * (polygon.get(2).getY() - polygon.get(1).getY());
         } else if (azimuthAngle == 90 || azimuthAngle == 270) {
-            height = Math.tan(slopeAngleRad) * (polygon.getPoint(1).getX() - polygon.getPoint(0).getX());
+            height = Math.tan(slopeAngleRad) * (polygon.get(1).getX() - polygon.get(0).getX());
         } else {
-            height = Math.tan(slopeAngleRad) * Math.sqrt(Math.pow(polygon.getPoint(1).getX() -
-                    polygon.getPoint(0).getX(), 2) + Math.pow(polygon.getPoint(2).getY() -
-                    polygon.getPoint(1).getY(), 2));
+            height = Math.tan(slopeAngleRad) * Math.sqrt(Math.pow(polygon.get(1).getX() -
+                    polygon.get(0).getX(), 2) + Math.pow(polygon.get(2).getY() -
+                    polygon.get(1).getY(), 2));
         }
+        QuaternionRotation azimuthRotation = QuaternionRotation.fromAxisAngle(Vector3D.of(0, 0, 1), Math.toRadians(azimuthAngle));
+        QuaternionRotation slopeRotation = QuaternionRotation.fromAxisAngle(Vector3D.of(1, 0, 0), slopeAngleRad);
 
-        for (int i = 0; i < polygon.size(); i++) {
-            Point point = polygon.getPoint(i);
-            double z = point.getZ();
-            double rotatedX = point.getX();
-            double rotatedY = point.getY();
-            if (((azimuthAngle == 0 || azimuthAngle == 360) && (i == 0 || i == 1)) ||
-                    ((azimuthAngle == 90) && (i == 0 || i == 3)) ||
-                    ((azimuthAngle == 180) && (i == 2 || i == 3)) ||
-                    ((azimuthAngle == 270) && (i == 1 || i == 2))) {
+
+        List<Vector3D> resultPolygon = new ArrayList<>();
+        for (Vector3D vertex : transformedPolygon) {
+            // Apply slope rotation first, then azimuth rotation
+            Vector3D rotatedVertex = slopeRotation.apply(vertex);
+
+            // Adjust Z-coordinate based on slope angle
+            double z = vertex.getZ();
+            if (((azimuthAngle == 0 || azimuthAngle == 360) && (vertex.equals(polygon.get(0)) || vertex.equals(polygon.get(1)))) ||
+                    ((azimuthAngle == 90) && (vertex.equals(polygon.get(0)) || vertex.equals(polygon.get(3)))) ||
+                    ((azimuthAngle == 180) && (vertex.equals(polygon.get(2)) || vertex.equals(polygon.get(3)))) ||
+                    ((azimuthAngle == 270) && (vertex.equals(polygon.get(1)) || vertex.equals(polygon.get(2))))) {
+
                 z += height;
+                resultPolygon.add(Vector3D.of(vertex.getX(), vertex.getY(), z));
+
+            } else if (((azimuthAngle == 0 || azimuthAngle == 360) && (vertex.equals(polygon.get(2)) || vertex.equals(polygon.get(3)))) ||
+                    ((azimuthAngle == 90) && (vertex.equals(polygon.get(1)) || vertex.equals(polygon.get(2)))) ||
+                    ((azimuthAngle == 180) && (vertex.equals(polygon.get(0)) || vertex.equals(polygon.get(1)))) ||
+                    ((azimuthAngle == 270) && (vertex.equals(polygon.get(0)) || vertex.equals(polygon.get(3))))) {
+
+                resultPolygon.add(Vector3D.of(vertex.getX(), vertex.getY(), z));
+
+            } else {
+                resultPolygon.add(Vector3D.of(rotatedVertex.getX(), rotatedVertex.getY(), z));
             }
+
+            // Add rotated and adjusted vertex to result polygon
+
+
             //
 //            if (
 //                    ((azimuthAngle > 0 && azimuthAngle <= 90) && (i == 0 )) ||
@@ -94,12 +125,11 @@ public class Main {
 //                z += height;
 //            }
 
-            resultPolygon.addPoint(new Point(Math.round(rotatedX * 100.0) / 100.0,
-                    Math.round(rotatedY * 100.0) / 100.0,
-                    Math.round(z * 100.0) / 100.0));
-        }
+//            resultPolygon.addPoint(new Point(Math.round(rotatedX * 100.0) / 100.0,
+//                    Math.round(rotatedY * 100.0) / 100.0,
+//                    Math.round(z * 100.0) / 100.0));
 
-        return resultPolygon;
+
+        }return resultPolygon;
     }
-
 }
